fc383f17729f05aa793395ebd153a2278d530e53
vini6569 <mvgrvinicius@gmail.com>
2026-09-04 21:31:09 -0300

    Remove redundant 'ipm' entry from main.py
    Removed redundant 'ipm' entry retrieval from resultado.

--- main.py
+++ main.py
@@ -654,14 +654,7 @@ def registrar_trajetoria(
             0.0
         ),
 
-        "ipm": resultado.get(
-            "ipm",
-            0.0
-        ),
-
-    
-
-
+        
 
 # ============================================================
 # REGISTRAR TRAJETÓRIA
